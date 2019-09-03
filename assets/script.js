c684e208757038f0c7d13b0aef228c01d8dc9ddf
MattyxBaguette <mathis.gadie@hetic.net>
2019-09-03 15:36:14 +0200

    :zap:

--- assets/script.js
+++ assets/script.js
@@ -16,18 +16,18 @@ document.addEventListener('mousemove', (e) => {
 
 // Scroll reveal
 
-ScrollReveal().reveal('.subtitle', { delay: 100 });
+ScrollReveal().reveal('.subtitle', { delay: 70 });
 
-ScrollReveal().reveal('.title', { delay: 100 });
+ScrollReveal().reveal('.title', { delay: 70 });
 
-ScrollReveal().reveal('.introduction', { delay: 100 });
+ScrollReveal().reveal('.introduction', { delay: 70 });
 
-ScrollReveal().reveal('.caption', { delay: 100 });
+ScrollReveal().reveal('.caption', { delay: 70 });
 
-ScrollReveal().reveal('.right-caption', { delay: 100 });
+ScrollReveal().reveal('.right-caption', { delay: 70 });
 
-ScrollReveal().reveal('.visual', { delay: 100 });
+ScrollReveal().reveal('.visual', { delay: 70 });
 
-ScrollReveal().reveal('.text', { delay: 100 });
+ScrollReveal().reveal('.text', { delay: 70 });
 
-ScrollReveal().reveal('.partners', { delay: 100, interval: 150 });
+ScrollReveal().reveal('.partners', { delay: 70, interval: 100 });
